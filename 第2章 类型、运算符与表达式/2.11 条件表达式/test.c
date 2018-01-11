7c3b1e257280f85ea9f0e91b7ae686ca0867f5af
范云飞 <786452470@qq.com>
2018-01-11 15:47:55 +0800

    2.11 条件表达式

---
+++ 第2章 类型、运算符与表达式/2.11 条件表达式/test.c
@@ -0,0 +1,25 @@
+/* 条件表达式 */
+
+
+
+/* 
+   常用的条件运算符：
+   （1）if else
+   （2）？ ：
+ */
+
+ if (/* condition */)
+ {
+ 	/* code */
+ }
+ else
+ {
+
+ }
+
+
+ int expr1 = 2;
+ int expr2 = 1;
+ int expr3 = 3;
+
+ expr2 = (expr1 > expr3) ? expr1 : expr3;
