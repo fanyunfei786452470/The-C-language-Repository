c7adad0250036422cb676b9d22bda8a1b1954d52
yunfei.fan <fyf786452470@gmail.com>
2020-05-16 16:28:51 +0800

    4.3 外部变量

---
+++ 第4章 函数与程序结构/4.3 外部变量/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h> /* for atof() */
+#define MAXOP 100 /* max size of operand or operator */ 
+#define NUMBER '0' /* signal that a number was found */
+int getop(char []); void push(double); double pop(void);
+int main(int argc, char const *argv[])
+{
+    for (int i = 0; i < MAXOP; i++)
+    {
+        printf("%d",i);
+    }
+    
+    return 0;
+}
