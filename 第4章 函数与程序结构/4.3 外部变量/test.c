8fa4ebbc2467e6681f32cc4009319a8a75dd8ef9
yunfei.fan <fyf786452470@gmail.com>
2020-08-20 13:48:55 +0800

    test vscode config

--- 第4章 函数与程序结构/4.3 外部变量/test.c
+++ 第4章 函数与程序结构/4.3 外部变量/test.c
@@ -5,10 +5,10 @@
 int getop(char []); void push(double); double pop(void);
 int main(int argc, char const *argv[])
 {
-    for (int i = 0; i < MAXOP; i++)
-    {
-        printf("%d",i);
-    }
+    // for (int i = 0; i < MAXOP; i++)
+    // {
+        printf("testvscodeconfig\n");
+    // }
     
     return 0;
 }
